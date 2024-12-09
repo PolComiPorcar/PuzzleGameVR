b4ba87af265fbd1730c200186809ec5b4e915b0e
Pol Comí <67558603+PolComiPorcar@users.noreply.github.com>
2024-12-09 18:51:35 +0100

    Event clau feta

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Key : MonoBehaviour
+{
+    public UnityEvent onEventTriggered;
+
+    public void TriggerEvent()
+    {
+        if (onEventTriggered != null)
+        {
+            onEventTriggered.Invoke();
+        }
+    }
+}
